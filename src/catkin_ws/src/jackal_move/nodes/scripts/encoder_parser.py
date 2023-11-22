4b4c30a7012445b479142a3529eb7aafc782a99f
fedeoli <lvofrc95@gmail.com>
2023-11-22 03:46:56 +0200

    dev: xacro vs tf issue

--- src/catkin_ws/src/jackal_move/nodes/scripts/encoder_parser.py
+++ src/catkin_ws/src/jackal_move/nodes/scripts/encoder_parser.py
@@ -3,25 +3,34 @@
 import rospy
 import move.jackal_move as jm
 import nav_msgs.msg
+from geometry_msgs.msg import TransformStamped
+import tf
 
 if __name__ == '__main__':
     
     try:        
         
         # Initializes a rospy node 
-        rospy.init_node('encoder_parser', anonymous=True)         
+        rospy.init_node('encoder_parser', anonymous=True)  
         rate = rospy.Rate(10)
+          
+        # Create a TransformBroadcaster object
+        broadcaster = tf.TransformBroadcaster()     
         
         feedback_topic = rospy.get_param('~feedback_topic', '')
         publish_topic = rospy.get_param('~publish_topic', '')
+        ns = rospy.get_param('~ns', '')
 
         # Publisher for the Odometry message
         odom_pub = rospy.Publisher(publish_topic, nav_msgs.msg.Odometry, queue_size=10)
 
         # Subscriber to the feedback topic
-        sub = rospy.Subscriber(feedback_topic, jm.Feedback, lambda data: jm.encoder_parser(data, odom_pub))
+        sub = rospy.Subscriber(feedback_topic, jm.Feedback, lambda data: jm.encoder_parser(data, odom_pub, broadcaster, ns))
 
-        rospy.spin()
+        # loop
+        while not rospy.is_shutdown():
+        
+            rate.sleep()
         
     except rospy.ROSInterruptException:
         rospy.loginfo("Encoder parser failed.")
