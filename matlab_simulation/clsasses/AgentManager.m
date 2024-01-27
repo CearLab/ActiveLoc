f9a29ac067e956a1d54c28dd9b6b72cbbff8680a
Federico Oliva <federico.oliva.it@gmail.com>
2024-01-27 19:16:27 +0100

    chore: comment the code

--- matlab_simulation/clsasses/AgentManager.m
+++ matlab_simulation/clsasses/AgentManager.m
@@ -1,67 +1,164 @@
+%% Agent manager class
+% file: AgentManager.m
+% author: Ido Sherf 
+% date: 22/01/2024
+% description: hndle class handling the creation of an agent manager for 
+% all teams on a Map. Specifically, this class is a coded as 
+% singleton, namely it is ensured that the class has only one instance and 
+% it is provided a way to access that instance from any point in the 
+% application. 
+% (see map class)
 classdef AgentManager < handle
+
+    % class properties (public)
     properties (Access = public)
+
+        % list of teams
         TeamList = {}
+
+        % counter
         agents_counter = 0
+
     end
 
+    % class methods (private)
     methods (Access = private)
+
+        % Private constructor for singleton
         function obj = AgentManager()
-            % Private constructor for singleton
         end
+
     end
 
+    % class methods (static)
     methods (Static)
+
+        % get a single class instance from everywhere in the application
         function singleObj = getInstance()
+
+            % define persistent variable (still exists after the call)
             persistent uniqueInstance
+
+            % if it is empty, call the constructor
             if isempty(uniqueInstance)
                 uniqueInstance = AgentManager();
             end
+
+            % assign the access point to the persistent var
             singleObj = uniqueInstance;
+
         end
+
     end
 
+    % class methods
     methods
+
+        % create an agent using Agent class
         function createAgent(obj, location, team_num, roll)
+
+            % get team ID
             team = obj.getTeam(team_num);
+
+            % increase number of agents 
             obj.agents_counter = obj.agents_counter + 1;
+
+            % call constructor for Agent class
             agent = Agent(obj.agents_counter,location,team,roll);
+
+            % check on agent role
             if strcmp(roll,'team_mate')
+
+                % add agent to the team (see Team class)
                 team.add_team_mate(agent)
+
             elseif strcmp(roll,'team_leader')
+
+                % set team leader (see Team class)
                 team.set_leader(agent)
             else
+
+                % raise error
                 error('roll should be team_mate or team_leader')
             end
         end
+
+        % create a new team with ID
+        function team = add_team(obj,team_num)
+
+            % call constructor from Team class
+            team = Team(team_num);
+
+            % add item to team list
+            obj.TeamList{end+1} = team;
+        end
+
+        % add team to the list if there is no team. If not, get the team
+        % handle
         function team = getTeam(obj,team_num)
+
+            % if there is no team, create one with ID
             if isempty(obj.TeamList)
+
+                % call class method
                 team = obj.add_team(team_num);
                 return
+
             end
+
+            % if teams already exist, get the handle from team ID
+            % cycle over team IDs. Maybe the name here is not so
+            % straightforward
             for t = 1:length(obj.TeamList)
+
+                % get the right one
                 if obj.TeamList{t}.team_number == team_num
+
+                    % return team handle
                     team = obj.TeamList{t};
                     return
                 end
+
             end
+
+            % if no team matches the number, create a new team
             team = obj.add_team(team_num);
         end
-        function team = add_team(obj,team_num)
-            team = Team(team_num);
-            obj.TeamList{end+1} = team;
-        end
+
+        % get all teams (why this if the property is public and there is 
+        % only an AgentManager?)
         function teams_list = get_all_teams(obj)
+
+            % access class property
             teams_list = obj.TeamList;
+
         end
+
+        % get all agents (see Team class)
         function agents = get_all_agent(obj)
+
+            % get all teams from class method
             teams = obj.get_all_teams();
+
+            % get number of teams
             n = numel(teams);
+
+            % init empty cell array
             agents = {};
+
+            % cycle over the teams
             for i = 1:n
+
+                % for each team get all agents
                 agent_list = teams{i}.get_all_agents();
+
+                % append agents
                 agents = [agents,agent_list];
+
             end
             
         end
+
     end
+    
 end
--- matlab_simulation/clsasses/Map.m
+++ matlab_simulation/clsasses/Map.m
@@ -1,84 +1,197 @@
+%% Map class
+% file: Map.m
+% author: Ido Sherf 
+% date: 22/01/2024
+% description: handle class handling the implementation and population of a
+% 2D map of an environment. Specifically, this class is a coded as 
+% singleton, namely it is ensured that the class has only one instance and 
+% it is provided a way to access that instance from any point in the 
+% application.
 classdef Map < handle
+
+    % properties of the cass
     properties
+
+        % list of objects: cell array 
         obs_list = {};
+
+        % dev
         % map_figure = figure("Name",'map','Tag','map_fig')
+
+        % region covered by the map [m]
         map_span = [-8 8; -8 8];
     end
+
+    % methods of the class (private)
     methods (Access = private)
+
+        % Private constructor for the singleton class
         function obj = Map()
-            % Private constructor for the singleton class
+            
         end
     end
+
+    % methods of the class (static)
     methods (Static)
+
+        % get a single class instance from everywhere in the application
         function singleObj = getInstance()
+
+            % define persistent variable (still exists after the call)
             persistent uniqueInstance
+
+            % if it is empty, call the constructor
             if isempty(uniqueInstance)
                 uniqueInstance = Map();
             end
+
+            % assign the access point to the persistent var
             singleObj = uniqueInstance;
         end
+
+        % graphic setup of the map plot
         function set_figure()
+
+            % find if there exist a figure (specific tag,name)
             f = findobj('tag','map_fig');
+
+            % if not, create it 
             if isempty(f)
                 f = figure("Name",'map','Tag','map_fig');
             end
+
+            % set base graphics
             figure(f);hold on;box on; grid on;
         end
     end
+
+    % methodsof the class
     methods
+
+        % add a polygon to the environment
         function add_polygon(obj,geo)
+
+            % call the obstacle method (see Obstacle class)
             obj.obs_list{end + 1} = Obstacle('polygon',geo);
+
         end
 
+        % plot on figure all obstacles
         function draw_all_obs(obj)
+
+            % get number of ostacles
             n = numel(obj.obs_list);
+
+            % call static method
             Map.set_figure();
+
+            % set environment limits from class property
             xlim(obj.map_span(1,:));ylim(obj.map_span(2,:))
+
+            % draw the obstacles (see Obstacle class)
             for i = 1:n
                 obj.obs_list{i}.draw_on_map()
             end
+
         end
+
+        % plot all teams of heterogeneous objects
         function draw_all_teams(obj)
+
+            % get the Agent managers
             manager = AgentManager.getInstance();
+
+            % get list of teams
             team_list = manager.get_all_teams();
+
+            % get number of teams
             n = numel(team_list);
+
+            % call static method
             Map.set_figure();
+
+            % set environment limits from class property
             xlim(obj.map_span(1,:));ylim(obj.map_span(2,:))
+
+            % draw the team (see team class)
             for i = 1:n
                team_list{i}.plot_team();
             end
+
         end
+
+        % get all the LOS pairs between teams 
+        % HERE WE SHOULD ADD SENSING RANGE
         function los_table = calc_los_map(obj)
+
+            % get the Agent managers
             manager = AgentManager.getInstance();
+
+            % get all the agents
             agents = manager.get_all_agent();
+
+            % get number of agents
             na = numel(agents);
+
+            % init emptu list
             los_table = [];
+
+            % cycle over upper triagle of naxna matrix (all pairs)
             for i = 1:(na - 1)
+
+                % no diagonal
                 ma = i + 1;
+
+                % get location and ID of Ai
                 i_loc = agents{i}.location;
                 i_num = agents{i}.agent_number;
+
+                % second cycle
                 for j = ma:na
+
+                    % get location and ID of Aj
                     j_loc = agents{j}.location;
                     j_num = agents{j}.agent_number;
+
+                    % store IDs and locs
                     los_table(end+1,:) = [i_num,j_num,i_loc,j_loc];
                 end
+
             end
+
         end
+
+        % draw LOS on the map
         function draw_los_map(obj)
+
+            % call static method
             Map.set_figure();
+
+            % compute LOS distances
             los_table = obj.calc_los_map();
+
+            % get number of pairs and get Ni (rows)
             n = size(los_table);
             n = n(1);
+
+            % for each Ai draw LOS
             for i = 1:n
+
+                % get locs
                 c_line = los_table(i,:);
+
+                % get origin and endpoint in XoY
                 x1 = c_line(3);
                 x2 = c_line(5);
                 y1 = c_line(4);
                 y2 = c_line(6);
+
+                % plot
                 plot([x1,x2],[y1,y2],'Color','k');
             end
-         
 
         end
+
     end
+    
 end
