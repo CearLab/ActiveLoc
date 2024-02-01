e1f8e3bb1dddf6266b582fc7576e1030a31f5e05
idosh0 <63654905+idosh0@users.noreply.github.com>
2024-02-01 12:35:50 +0200

    matrix calculation

---
+++ matlab_simulation/utils/calcAdjacencyMatrix.m
@@ -0,0 +1,17 @@
+%% calc adjacency matrix
+% file: calcIncidenceMatrix.m
+% author: Ido Sherf 
+% date: 01/02/2024
+% description: calculate the adjacency matrix for given los table 
+function A = calcAdjacencyMatrix(los_table,agents_list)
+n = length(agents_list); % A is a |V|*|V| matrix
+A = zeros(n);
+for ie = 1:size(los_table,1)
+    e = los_table(ie,:);
+    v1 = e(1);
+    v2 = e(2);
+    A(v1,v2) = 1;
+    A(v2,v1) = 1;
+end
+end
+
