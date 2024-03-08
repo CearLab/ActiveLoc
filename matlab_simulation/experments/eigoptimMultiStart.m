56013ae778874d7f6c6f3fe90d64ea59c494d690
Federico Oliva <federico.oliva.it@gmail.com>
2024-03-08 13:36:33 +0100

    dev: working on removing agents policy for global rigidity

--- matlab_simulation/experments/eigoptimMultiStart.m
+++ matlab_simulation/experments/eigoptimMultiStart.m
@@ -20,12 +20,13 @@ map = Map.getInstance();
 manager = AgentManager.getInstance();
 manager.reset();
 
-m = 7; % number of agents
+m = 5; % number of agents
 p = 2;
 
 % define range for RD sensors
-UWBrange = 8;
+UWBrange = 10;
 sigmarand = 10;
+Dminthresh = 0*0.5*UWBrange;
 
 % define exit condition
 isrigid = 0;
@@ -38,23 +39,32 @@ leaderID = 1;
 % init
 iter = 0;
 iteropt = 0;
+optattempt = 0;
+maxattempt = 1000;
 targetfeasible = 100;
-targetimproved = 5;
+targetimproved = 10;
 Jold = Inf;
 
 % start counter
 tic
 disp('Find initial condition')
-while (feasibleimproved<targetimproved) && (feasiblefound<targetfeasible) %&& iter < 1
+while (optattempt<maxattempt) && (feasibleimproved<targetimproved) && (feasiblefound<targetfeasible) %&& iter < 1
 
     % iter
-    iter = iter + 1;
+    iter = iter + 1;    
 
     % disp
     clc
     disp(['Feasible improved ', num2str(feasibleimproved)]);
     disp(['Feasible found ', num2str(feasiblefound)]);
     disp(['X0 attempt ', num2str(iter)]);
+
+    % reset manager
+    manager.reset();
+    manager.WS.eigThresh = 1e-5;
+    manager.WS.p = p;
+    manager.WS.m = m;
+    manager.WS.Dminthresh = Dminthresh;
 
     % define initial condition
     agents_pos = zeros(m,p);
@@ -70,10 +80,7 @@ while (feasibleimproved<targetimproved) && (feasiblefound<targetfeasible) %&& it
     
     % max over the map span
     agents_pos(:,1) = max(min(agents_pos(:,1),0.9*map.map_span(1,2)),0.9*map.map_span(1,1));
-    agents_pos(:,2) = max(min(agents_pos(:,2),0.9*map.map_span(2,2)),0.9*map.map_span(2,1));
-
-    % reset manager
-    manager.reset();
+    agents_pos(:,2) = max(min(agents_pos(:,2),0.9*map.map_span(2,2)),0.9*map.map_span(2,1));    
 
     % assign positions of initial team
     for i = 1:m
@@ -103,6 +110,7 @@ while (feasibleimproved<targetimproved) && (feasiblefound<targetfeasible) %&& it
     if ~isempty(los_table)
         D = los_table(:,5:5+p-1) - los_table(:,5+p:5+2*p-1);
         Dmin = min(vecnorm(D,2,2));
+        Dmax = max(vecnorm(D,2,2));
     else
         Dmin = 0;
     end
@@ -111,40 +119,36 @@ while (feasibleimproved<targetimproved) && (feasiblefound<targetfeasible) %&& it
     R = calcRigitdyMatrix(los_table,agents_list);
 
     % get nonzero eigs
-    e = eig(R'*R);
-
-    % get # nnz elements
-    pos = find(abs(e) < 1e-10);
-
-    % set Dminthresh
-    Dminthresh = 0*0.25*max(max(map.map_span));
-    if isinf(Dminthresh)
-        Dminthresh = 0;
-    end
+    e = eig(R'*R);   
+    pos = find(abs(e) < manager.WS.eigThresh);   
 
     % exit condition
-    if (numel(pos) < 4) && (Dmin > Dminthresh)
+    if (numel(pos) == 3)
         isrigid = 1;   
     else
         isrigid = 0;
     end
 
+    % DminThresh
+    DminThresh = manager.WS.Dminthresh;
+    DmaxThresh = 2*agents{1}.sensors.UWB.max_range;
+
+    A = calcAdjacencyMatrix(los_table,agents_list);
+    [allConn, A] = agents{1}.checkConnectivity(A);
+
     % now run optimization
-    if isrigid
+    if (allConn) && (Dmin > DminThresh)
         
         % update
         iteropt = iteropt + 1;
+        optattempt = iteropt;
 
         % disp
-        disp(['optimization attempt ', num2str(iteropt)]);
+        disp(['optimization attempt ', num2str(iteropt)]);        
 
-        % init manager
-        manager.WS.p = p;
-        manager.WS.m = m;
-        
         % copy initial condition as second team: this will be the one we optimize on
         for i = 1:m
-            if i==leaderID
+            if i==leaderID                
                 manager.createAgent(agents_pos(i,:),2,'team_leader'); %create the leader
             else
                 manager.createAgent(agents_pos(i,:),2,'team_mate'); %create the followers
@@ -152,23 +156,16 @@ while (feasibleimproved<targetimproved) && (feasiblefound<targetfeasible) %&& it
         end
         
         % get the team you just created
-        team = manager.team_list{2};
+        teamInit = manager.team_list{2};
         
         % get all agents from the team 
-        agents = {team.team_mates{1:end}};
-        agents = {agents{1:team.leader.agent_number-1-m} team.leader agents{team.leader.agent_number-m:end}};
+        agentsInit = {teamInit.team_mates{1:end}};
+        agentsInit = {agentsInit{1:teamInit.leader.agent_number-1-m} teamInit.leader agentsInit{teamInit.leader.agent_number-m:end}};
         
         % set sensors
         for i = 1:m        
-            agents{i}.sensors.UWB = Sensor(agents{i}.agent_number,'range',UWBrange);        
-        end
-
-        % test J0
-
-        % set store options for cost function
-        manager.WS.J = [];
-        manager.WS.X = [];
-        manager.WS.CN = [];
+            agentsInit{i}.sensors.UWB = Sensor(agentsInit{i}.agent_number,'range',UWBrange);        
+        end        
         
         X0 = reshape(agents_pos',size(agents_pos,1)*size(agents_pos,2),1);
         J0 = cost_function(X0,p);
@@ -185,23 +182,15 @@ while (feasibleimproved<targetimproved) && (feasiblefound<targetfeasible) %&& it
                                 'InitialMeshSize',1e2, ...
                                 'UseParallel', false, ...
                                 'MaxFunctionEvaluations', 1e10, ...
-                                'ConstraintTolerance', 1e-4, ...
+                                'ConstraintTolerance', 1e-1, ...
                                 'StepTolerance', 1e-10, ...
                                 'Cache', 'On', ...
-                                'MaxTime', 300, ...
-                                'Algorithm','classic');
-        
-        % optimization - fmincon
-        % options = optimoptions( 'fmincon', ...
-        %                         'Algorithm','interior-point', ...
-        %                         'MaxFunctionEvaluations',1e5, ...
-        %                         'MaxIterations',1e3, ...
-        %                         'OptimalityTolerance',1e-10, ...
-        %                         'StepTolerance', 1e-10, ...
-        %                         'FunctionTolerance',1e-10);
+                                'MaxTime', 30, ...
+                                'Algorithm','classic');       
         
         % optimization - fmincon
         % options = optimset( 'Display','off', ...
+        %                     'Algorithm','interior-point', ...                                                        
         %                     'TolX', 1e-10, ...
         %                     'TolFun', 1e-10, ...
         %                     'MaxFunEvals', 1e10, ...
@@ -217,17 +206,38 @@ while (feasibleimproved<targetimproved) && (feasiblefound<targetfeasible) %&& it
                             [],                     ...
                             [],                     ...
                             [],                     ...
-                            1*LB,                   ...
-                            1*UB,                   ...
+                            Inf*LB,                   ...
+                            Inf*UB,                   ...
                             @(x)nonlcon(x,p), ...
                             options);
 
+        % get the team you just optimized
+        teamOpt = manager.team_list{1};
+        
+        % get all agents from the team 
+        agentsOpt = {teamOpt.team_mates{1:end}};
+        agentsOpt = {agentsOpt{1:teamOpt.leader.agent_number-1} teamOpt.leader agentsOpt{teamOpt.leader.agent_number:end}};
+
         if EXITFLAG ~= -2
+            % found a feasible solution
             feasiblefound = feasiblefound + 1;
+
+            % is it better than before?
             if J < Jold
                 feasibleimproved = feasibleimproved + 1;
-                Jold = J;
-                X = Xtmp;
+
+                % store stuff  
+                J0old = J0;   
+                X0old = X0;                             
+                [los_table0_old,agents_list0_old] = calcLosMap(agentsInit,'UWB');    
+                R0_old = calcRigitdyMatrix(los_table0_old,agents_list0_old);
+                e0_old = eig(R0_old'*R0_old);                
+
+                Jold = J;   
+                Xold = Xtmp;   
+                [los_table_old,agents_list_old] = calcLosMap(agentsOpt,'UWB');    
+                R_old = calcRigitdyMatrix(los_table_old,agents_list_old);
+                e_old = eig(R_old'*R_old);                
             end        
         end
 
@@ -236,23 +246,62 @@ while (feasibleimproved<targetimproved) && (feasiblefound<targetfeasible) %&& it
 end
 topt = toc;
 
-% rotation and translation (nodes 1-2)
-% reshape agents positions
-X0tmp = reshape(X0,p,floor(numel(X0)/p))';
-Xtmp = reshape(X,p,floor(numel(X)/p))';
+%% reshape agents positions
+% set initial condition
+X0tmp = reshape(X0old,p,floor(numel(X0old)/p))';
+tmp = X0tmp(leaderID,:);
+X0tmp = X0tmp - tmp;
+
+% set optimized condition
+Xtmp = reshape(Xold,p,floor(numel(Xold)/p))';
 tmp = Xtmp(leaderID,:);
 Xtmp = Xtmp - tmp;
-V10 = X0tmp(1,:);
-V1 = Xtmp(1,:);
-DP = V1-V10;
 
-% translation
-% Xtmp = Xtmp - DP;
+% reset everything and recreate teams
+% reset manager
+manager.reset();
+manager.WS.eigThresh = 1e-5;
+manager.WS.p = p;
+manager.WS.m = m;           
+manager.WS.Dminthresh = Dminthresh;
 
-% init
-Xbest = reshape(Xtmp',size(Xtmp,1)*size(Xtmp,2),1);
+% assign positions of optimized team
+for i = 1:m
+    if i==leaderID
+        manager.createAgent(Xtmp(i,:),1,'team_leader'); %create the leader        
+    else        
+        manager.createAgent(Xtmp(i,:),1,'team_mate'); %create the followers
+    end        
+end
 
-[c, ceq] = nonlcon(Xbest,p);
+% assign position for init team
+for i = 1:m
+    if i==leaderID        
+        manager.createAgent(X0tmp(i,:),2,'team_leader'); %create the leader
+    else
+        manager.createAgent(X0tmp(i,:),2,'team_mate'); %create the followers        
+    end        
+end
+
+% get the team you just created
+teamOpt = manager.team_list{1};
+teamInit = manager.team_list{2};
+
+% get all agents from the team optimized 
+agentsOpt = {teamOpt.team_mates{1:end}};
+agentsOpt = {agentsOpt{1:teamOpt.leader.agent_number-1} teamOpt.leader agentsOpt{teamOpt.leader.agent_number:end}};
+
+% get all agents from the team initialized
+agentsInit = {teamInit.team_mates{1:end}};
+agentsInit = {agentsInit{1:teamInit.leader.agent_number-1-m} teamInit.leader agentsInit{teamInit.leader.agent_number-m:end}};
+
+
+% set sensors
+for i = 1:m        
+    agentsOpt{i}.sensors.UWB = Sensor(agentsOpt{i}.agent_number,'range',UWBrange);        
+    agentsInit{i}.sensors.UWB = Sensor(agentsInit{i}.agent_number,'range',UWBrange);        
+end     
+
               
 %% plot section
 
@@ -291,41 +340,14 @@ fill([  map.map_span(1,1) map.map_span(1,1) map.map_span(1,2) map.map_span(1,2)]
      'FaceAlpha',0.1, ...
      'LineWidth',1.5);
 
-% get the team you just created
-team = manager.team_list{2};    
-
-% get all agents from the team 
-agents0 = {team.team_mates{1:end}};    
-% get m
-m = manager.WS.m;
-% insert leader in right position
-agents0 = {agents0{1:team.leader.agent_number-1-m} team.leader agents0{team.leader.agent_number-m:end}};
-
-% get agents summary tables
-[los_table0,agents_list0] = calcLosMap(agents0,'UWB');
-
-teams{2}.plotTeam(s1,9);
-drawLosMap(los_table0,s1,9);
+teamInit.plotTeam(s1,9);
+drawLosMap(los_table0_old,s1,9);
 xlim(scaleaxis*map.map_span(1,:)); ylim(scaleaxis*map.map_span(2,:));
 axis equal
 
-% team 1 - optimized
-% f1 = figure(2);
-% hold on; box on; grid on;
-
-% get the team you just created
-team = manager.team_list{1};    
-
-% get all agents from the team 
-agents = {team.team_mates{1:end}};    
-% insert leader in right position
-agents = {agents{1:team.leader.agent_number-1} team.leader agents{team.leader.agent_number:end}};
-
-% get agents summary tables
-[los_table,agents_list] = calcLosMap(agents,'UWB');
-
-teams{1}.plotTeam(s2,2);
-drawLosMap(los_table,s2,2);
+% team 2 - optimized
+teamOpt.plotTeam(s2,2);
+drawLosMap(los_table_old,s2,2);
 xlim(scaleaxis*map.map_span(1,:)); ylim(scaleaxis*map.map_span(2,:));
 axis equal
 
@@ -333,3 +355,7 @@ xlabel('X axis')
 ylabel('Y axis')
 
 warning('on','all')
+
+%% silly
+load('handel.mat')
+sound(y,Fs);
--- matlab_simulation/utils/cost_function.m
+++ matlab_simulation/utils/cost_function.m
@@ -18,46 +18,76 @@ function J = cost_function(x,p)
     m = manager.WS.m;
 
     % get the team you just created
-    team = manager.team_list{1};    
+    teamOpt = manager.team_list{1};
     
-    % get all agents from the team 
-    agents = {team.team_mates{1:end}};    
-    % insert leader in right position
-    agents = {agents{1:team.leader.agent_number-1} team.leader agents{team.leader.agent_number:end}};
+    % get all agents from the team optimized 
+    agentsOpt = {teamOpt.team_mates{1:end}};
+    agentsOpt = {agentsOpt{1:teamOpt.leader.agent_number-1} teamOpt.leader agentsOpt{teamOpt.leader.agent_number:end}};
 
 
     % reassign agents position
     for i=1:m
-        agents{i}.location = xmat(i,:);
+        agentsOpt{i}.location = xmat(i,:);
     end
 
     % get agents summary tables
-    [los_table,agents_list] = calcLosMap(agents,'UWB');
+    [los_table,agents_list] = calcLosMap(agentsOpt,'UWB');
 
     % get rigidity matrix 
     R = calcRigitdyMatrix(los_table,agents_list);
 
-    % get nonzero eigs
-    e = eig(R'*R);
-    pos = find(abs(e) < 1e-10);
+    % J rigidity   
+    e = eig(R'*R);    
+    pos = find(abs(e) < 1e-5);    
+    isrigid = (numel(pos)==3);              
     e(pos) = [];
-    
-    J = 1/min(e);
-    % J = min(e)/max(e);
+    JR = -min(e);
+    % JR = 1/(1 + min(e));    
+    % JR = min(e)/max(e);
 
-    if ~isscalar(J)
+    % is connected
+    % A = calcAdjacencyMatrix(los_table,agents_list);
+    % [allConn, A] = agentsOpt{1}.checkConnectivity(A);
+
+           
+
+    % get agents_pos
+    % agents_pos = agents_list(:,2:3);
+    % sigma = std(agents_pos,1);
+    % sigmacost = sigma*sigma';
+    % JS = 1/(1+sigmacost);
+    if ~isempty(los_table)
+        JS = 0;
+        N = size(los_table,1);  
+
+        % spreadiness term
+        tmp = los_table(:,5:5+p-1) - los_table(:,5+p:5+2*p-1);
+        JS = JS + sum(tmp(:,1).^2.*tmp(:,2).^2);    
+        JS = -JS/N^6;
+
+        % symmetry term        
+    else
+        JS = 0;
+    end
+    
+
+    % penalty on Dmin
+    % if ~isempty(los_table)
+    %     D = los_table(:,5:5+p-1) - los_table(:,5+p:5+2*p-1);
+    %     Dmin = min(vecnorm(D,2,2));
+    % else
+    %     Dmin = 0;
+    % end
+    % JD = 1/(1+Dmin);
+    JD = 0;
+    
+
+    % normalize
+    % minterm = min(JD,JR);    
+    J = 1*JR + 0*JD + 0*JS;
+
+    if isempty(J)
         J = Inf;
     end
-
-    % store cost function
-    manager.WS.J(end+1) = J;
-
-    % store condition number
-    if ~isempty(e)
-        manager.WS.CN(end+1) = max(e)/min(e);
-    end
-
-    % store optimized var
-    manager.WS.X(:,end+1) = x;
 
 end
