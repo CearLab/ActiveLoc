5e0ea0b7ebd8f81f7fc7b1b577ddf8f1d25477f7
Federico Oliva <federico.oliva.it@gmail.com>
2024-07-25 10:09:31 +0300

    dev: multi agent visualize

--- src/catkin_ws/src/jackal_custom/nodes/scripts/UGV_vis_line_MAS.py
+++ src/catkin_ws/src/jackal_custom/nodes/scripts/UGV_vis_line_MAS.py
@@ -11,17 +11,18 @@ if __name__ == '__main__':
         # ros::init() the node
         rospy.init_node('MAS_line_visualization', anonymous=True)
         
+        # get params        
+        odom_topic = rospy.get_param('~odom_topic', '')
+        anchors_topic = ''
+        pub_topic = rospy.get_param('~pub_topic', '')        
+        color = rospy.get_param('~color', '')
+        
         # instance jackal_move
-        jvis_instance = JackalVis()
+        jvis_instance = JackalVis(anchors_topic,odom_topic,pub_topic,color)        
         rospy.Rate(jvis_instance.RATE)
         
-        # get params        
-        color = rospy.get_param('~color', '')
-        subtopic = rospy.get_param('~subtopic', '')
-        pubtopic = rospy.get_param('~pubtopic', '')
-        
         # call talker
-        jvis_instance.publish_line_agents(color,subtopic,pubtopic)
+        jvis_instance.publish_line_agents()
         rospy.spin()
         
     except rospy.ROSInterruptException:
